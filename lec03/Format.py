794d13564b4715ac5e3ee22ecd930abb0a275b03
Seungwoo Kang <swkang@koreatech.ac.kr>
2017-09-07 20:46:46 +0900

    lec03 example codes

---
+++ lec03/Format.py
@@ -0,0 +1,29 @@
+print(format(57.467657, "10.2f"))
+print(format(12345678.923, "10.2f"))
+print(format(57.4, "10.2f"))
+print(format(57, "10.2f"))
+
+print(format(57.467657, "10.2e"))
+print(format(0.0033923, "10.2e"))
+print(format(57.4, "10.2e"))
+print(format(57, "10.2e"))
+
+print(format(0.53457, "10.2%"))
+print(format(0.0033923, "10.2%"))
+print(format(7.4, "10.2%"))
+print(format(57, "10.2%"))
+
+print(format(57.467657, "10.2f"))
+print(format(57.467657, "<10.2f"))
+
+print(format(59832, "10d"))
+print(format(59832, "<10d"))
+print(format(59832, "10x"))
+print(format(59832, "<10x"))
+print(format(59832, "10o"))
+print(format(59832, "<10o"))
+
+print(format("파이썬에 오신 것을 환영합니다.", "20s"))
+print(format("파이썬에 오신 것을 환영합니다.", "<20s"))
+print(format("파이썬에 오신 것을 환영합니다.", ">20s"))
+print(format("파이썬과 자바에 오신 것을 환영합니다.", ">20s"))
